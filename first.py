ceaeac675cf9387fbbcb26f18246ada4e8cfcd05
DillibabuDega <159100358+DillibabuDega@users.noreply.github.com>
2024-02-06 12:41:47 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,30 @@
+#hello world
+print("Hello World")
+
+#sum of two numbers
+a = int(input("Enter a number "))
+b = int(input("Enter a number "))
+print(a+b)
+
+#Division of two realnumbers
+x = input("Enter a real numbers")
+y = input("Enter a real numbers")
+if (float(x)==True and float(y)==True):
+	print(float(x)/float(y))
+else :
+	print("Enter a valid number")
+	
+#multiplication of two lists	
+x =[1,2,3]
+y=[4,5,6]
+z = []
+if(len(x)==len(y)):
+	for i in range(len(x)):
+		z.append(x[i]*y[i])
+	print(z)
+else:
+	return 0
+
+#Determinant of 3x3 matrix
+
+	
